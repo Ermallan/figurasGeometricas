403c2dc108b75432a1e8bf2af80a59c82070ab10
Allanerm23 <allanerm23@gmail.com>
2024-03-14 20:13:41 -0600

    curso java

--- src/FigurasGeometricas.java
+++ src/FigurasGeometricas.java
@@ -7,6 +7,7 @@ public abstract class  FigurasGeometricas {
     public  double getValor1(){
         return valor1;
     }
+    //
     public void setValor1(double valor1){
         this.valor1=valor1;
     }
